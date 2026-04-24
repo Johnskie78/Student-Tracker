537190bdb82a5cf02fc2422359fd0a69b2710ddd
nagret71 <nagret71@gmail.com>
2026-04-24 08:56:43 +0800

    fix: update author identity

--- my-app/renderer/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-export default {
-  output: 'export',
-  distDir: process.env.NODE_ENV === 'production' ? '../app' : '.next',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-}
